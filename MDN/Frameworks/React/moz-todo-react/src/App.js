bd224ed40c1390716ca826add6d7ad6d45d1a163
BugaCo <li_yizhe@icloud.com>
2020-12-25 18:01:52 +0800

    some mdn react

--- MDN/Frameworks/React/moz-todo-react/src/App.js
+++ MDN/Frameworks/React/moz-todo-react/src/App.js
@@ -1,16 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import Todo from "./components/Todo";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
+import React, { useState } from "react";
+import { nanoid } from "nanoid";
+function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
 
-function App() {
+  const taskList = tasks.map((task) => (
+    <Todo
+      name={task.name}
+      completed={task.completed}
+      id={task.id}
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+      editTask={editTask}
+    />
+  ));
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+  function addTask(name) {
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  function deleteTask(id) {
+    console.log(id)
+    setTasks(tasks.filter(task => task.id !== id))
+  }
+
+  function editTask(id, newName) {
+    console.log(id, newName)
+    const editedTasks = tasks.map( task => {
+      if (id === task.id) {
+        return {...task, name: newName}
+      }
+      return task
+    })
+    setTasks(editedTasks)
+  }
+
+  function toggleTaskCompleted(id) {
+
+    const updatedTasks = tasks.map(task => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        // const rst = {...task, completed: !task.completed}
+        // return rst
+        task.completed = !task.completed
+
+        return task
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, World!
-        </p>
-        
-      </header>
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">{headingText}</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
     </div>
   );
 }
